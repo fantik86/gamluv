ce4684a4e766ae0e9e94afbee0d1387ff553c131
Richter <fantik@fantik.online>
2024-05-01 15:02:54 +0300

    Small changes

--- src/executer.c
+++ src/executer.c
@@ -7,11 +7,16 @@
 void execute_instruction(code_instruction* instruction) {
   int min = INT_MIN;
   int index = instruction->opcode_index;
-  if (*(int*)instruction->arg1 == min) {
-    opcode_table[index].function_ptr(&min, &min); /* No arguments */
-  } else if (*(int*)instruction->arg2 == INT_MIN) {
-    opcode_table[index].function_ptr((int*)(instruction->arg1), &min); /* 1 argument */
-  } else {
-    opcode_table[index].function_ptr((int*)(instruction->arg1), (int*)(instruction->arg2)); /* 2 arguments */
-  }
+  int* arg1_ptr = (int*)instruction->arg1;
+  void (*opcode_call)();
+  opcode_call = opcode_table[index].function_ptr;
+  if (*(int*)&instruction->arg1 == min)
+    /* No arguments */
+    opcode_call(&min, &min); 
+  else if (*(int*)&instruction->arg2 == min)
+    /* 1 argument */
+    opcode_call((int*)&instruction->arg1, &min); 
+  else
+    /* 2 arguments */
+    opcode_call((int*)instruction->arg1, (int*)instruction->arg2); 
 }
